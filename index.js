1e7712f2bd42e94dc46fee122573156989cc8f4e
Ryan McVeety <rpm0618@gmail.com>
2016-02-19 22:27:51 -0500

    Added environment variable provider

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const LiteralProvider = require('./lib/providers/literal-provider');
 const CliProvider = require('./lib/providers/cli-provider');
 const FileProvider = require('./lib/providers/file-provider');
+const EnvProvider = require('./lib/providers/env-provider');
 const Configurator = require('./lib/configurator');
 
 function main() {
@@ -10,10 +11,12 @@ function main() {
     configurator.register('literal', LiteralProvider);
     configurator.register('cli', CliProvider);
     configurator.register('file', FileProvider);
+    configurator.register('env', EnvProvider);
 
     configurator.use('literal', {Hello: 'World'});
     configurator.use('cli');
     configurator.use('file', {fromConfig: 'file', skipMissing: true});
+    configurator.use('env', {match: /compute.name/, lowerCase: true});
 
     configurator.build().then(function(config) {
         console.log(config);
