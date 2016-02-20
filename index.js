442d0244d3ce0681bb7d09b0ede13b963577fbd8
Ryan McVeety <rpm0618@gmail.com>
2016-02-19 20:59:13 -0500

    Add file provider

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict'
 
-var LiteralProvider = require('./lib/providers/literal-provider');
-var CliProvider = require('./lib/providers/cli-provider');
-var Configurator = require('./lib/configurator');
+const LiteralProvider = require('./lib/providers/literal-provider');
+const CliProvider = require('./lib/providers/cli-provider');
+const FileProvider = require('./lib/providers/file-provider');
+const Configurator = require('./lib/configurator');
 
 function main() {
     let configurator = new Configurator();
     configurator.register('literal', LiteralProvider);
     configurator.register('cli', CliProvider);
+    configurator.register('file', FileProvider);
 
     configurator.use('literal', {Hello: 'World'});
     configurator.use('cli');
+    configurator.use('file', {fromConfig: 'file', skipMissing: true});
 
     configurator.build().then(function(config) {
         console.log(config);
