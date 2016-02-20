d9095f27dad7baa55fc8cfc85370980b8c4c7806
Ryan McVeety <rpm0618@gmail.com>
2016-02-19 19:51:59 -0500

    Add cli configuration provider

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict'
 
 var LiteralProvider = require('./lib/providers/literal-provider');
-var Configurator = require('./lib/configurator')
+var CliProvider = require('./lib/providers/cli-provider');
+var Configurator = require('./lib/configurator');
 
 function main() {
     let configurator = new Configurator();
     configurator.register('literal', LiteralProvider);
+    configurator.register('cli', CliProvider);
 
     configurator.use('literal', {Hello: 'World'});
+    configurator.use('cli');
 
     configurator.build().then(function(config) {
         console.log(config);
