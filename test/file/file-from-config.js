4da4178e3667e7ba0fce833a42ea204e21e7cb87
Ryan McVeety <rpm0618@gmail.com>
2016-02-22 19:45:44 -0500

    more tests, better merging (merge sub-objects correctly)

---
+++ test/file/file-from-config.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import Configurator from '../../index';
+import test from 'ava';
+import path from 'path';
+
+test('file', async t => {
+    const fileToUse = path.join(__dirname, 'data/test.json');
+    const expected = {
+        "test": 1,
+        "test2": [3, 4, 5],
+        "test3": {
+            "a": 1,
+            "b": 2
+        },
+        "Hello": "BEEF",
+        fileToUse: fileToUse
+    };
+
+    Configurator.use('literal', {fileToUse});
+    Configurator.use('file', {fromConfig: 'fileToUse'});
+    const config = await Configurator.build();
+
+    t.same(expected, config);
+})
---
+++ test/file/file.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import Configurator from '../../index';
+import test from 'ava';
+import path from 'path';
+
+test('file', async t => {
+    const expected = {
+        "test": 1,
+        "test2": [3, 4, 5],
+        "test3": {
+            "a": 1,
+            "b": 2
+        },
+        "Hello": "BEEF"
+    };
+
+    Configurator.use('file', path.join(__dirname, 'data/test.json'));
+    const config = await Configurator.build();
+
+    t.same(expected, config);
+});
