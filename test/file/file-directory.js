4b014396f008afddcf6f3e1924e882583e1bdf3d
Ryan McVeety <rpm0618@gmail.com>
2016-02-23 21:51:35 -0500

    If FileProvider is given a directory, parse all of the files in that directory and combine them

---
+++ test/file/file-directory.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import Configurator from '../../index';
+import test from 'ava';
+import path from 'path';
+
+test('file-directory', async t => {
+    const expected = {
+        "test": 1,
+        "test2": [3, 4, 5],
+        "test3": {
+            "a": 1,
+            "b": 2,
+            "c": 3
+        },
+        "test4": "Chicken",
+        "test5": "A large amount of people find spiders scary",
+        "Hello": "BEEF"
+    };
+
+    Configurator.use('file', path.join(__dirname, 'data/'));
+    const config = await Configurator.build();
+
+    t.same(expected, config);
+});
